d8eac9fa6531fea4c7b8767bf6aad3b3eea12502
KingMidas74 <kingmidas1992@gmail.com>
2018-07-14 20:47:43 +0500

    add implicit constructor for point and file reader

--- GrahamAlg/Point.cs
+++ GrahamAlg/Point.cs
@@ -7,6 +7,12 @@ namespace GrahamAlg
         public Int32 X { get; set; } = 0;
         public Int32 Y { get; set; } = 0;
 
+        public static implicit operator Point(string value)
+        {
+            var values = value.Split(';');
+            return new Point { X = Int32.Parse(values[0]), Y = Int32.Parse(values[1]) };
+        }
+
         public override bool Equals(object obj) => (obj is Point) && (obj as Point).X == X && (obj as Point).Y == Y;
     }
 }
---
+++ GrahamAlg/PointCreator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace GrahamAlg
+{
+    public class PointCreator
+    {
+        public static IEnumerable<Point> ReadPointsFromFile(String filePath)
+        {
+            return File.ReadAllLines(filePath).Select(line => (Point)line).ToList();
+        }
+    }
+}
