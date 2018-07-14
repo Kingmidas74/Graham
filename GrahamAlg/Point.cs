23dd98473951012409a4902ed7c9b1348a4e8fd9
KingMidas74 <kingmidas1992@gmail.com>
2018-07-14 20:44:08 +0500

    add class library for logic and point class

---
+++ GrahamAlg/Point.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GrahamAlg
+{
+    public class Point
+    {
+        public Int32 X { get; set; } = 0;
+        public Int32 Y { get; set; } = 0;
+
+        public override bool Equals(object obj) => (obj is Point) && (obj as Point).X == X && (obj as Point).Y == Y;
+    }
+}
